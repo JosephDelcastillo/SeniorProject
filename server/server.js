d643c156c4d3e4c0e0665ff13af82e5f122b08c9
Joseph Delcastillo III <delcastillo3j@gmail.com>
2022-10-24 15:54:51 -0400

    Server Preparations

--- server/server.js
+++ server/server.js
@@ -6,7 +6,7 @@ const express = require("express");
 const data = require('./controllers/Data')
 
 // Initializations 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 const app = express();
 const Data_Controller = data;
 
