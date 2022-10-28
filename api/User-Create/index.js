3478a06315911e404671cbc0c0a59fe57057489e
Joseph Delcastillo III <delcastillo3j@gmail.com>
2022-10-28 09:12:36 -0400

    Simplification for deploy testing

--- api/User-Create/index.js
+++ api/User-Create/index.js
@@ -1,19 +1,22 @@
 // Imports 
-const service = require('../services/Users');
+// const service = require('../services/Users');
 
 module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
 
-    const id = await service.GetUsersTableId();
-    
+    // const id = await service.GetUsersTableId();
     // TODO: Replace With Validation Service 
     const valid = {
         success: (req.query && req.query.id), 
-        message: "Please pass a name on the query string or in the request body"
+        message: "Please pass a id on the query string or in the request body"
     }
 
     if (valid.success) { 
-        service.Create(context, req.query)
+        context.res = {
+            status: 200,
+            body: req.query.id
+        };
+        // service.Create(context, req.query)
     }
     else {
         context.res = {
--- api/lib/User.js
+++ api/lib/User.js
@@ -1,16 +1,16 @@
-const USER_ROLES = {
-    Admin: 'Administrator', Staff: 'Staff'
-}
+// const USER_ROLES = {
+//     Admin: 'Administrator', Staff: 'Staff'
+// }
 
-function User ({ id, name, email, role, pass, salt, emailToken, emailTokenTime }) {
-    this.id = id ?? 0;
-    this.name = name;
-    this.email = email;
-    this.role = role ?? USER_ROLES.Staff;
-    this.pass = pass;
-    this.salt = salt;
-    this.emailToken = emailToken ?? '';
-    this.emailTokenTime = emailTokenTime ?? '';
-}
+// function User ({ id, name, email, role, pass, salt, emailToken, emailTokenTime }) {
+//     this.id = id ?? 0;
+//     this.name = name;
+//     this.email = email;
+//     this.role = role ?? USER_ROLES.Staff;
+//     this.pass = pass;
+//     this.salt = salt;
+//     this.emailToken = emailToken ?? '';
+//     this.emailTokenTime = emailTokenTime ?? '';
+// }
 
-module.exports = { User, USER_ROLES }
+// module.exports = { User, USER_ROLES }
