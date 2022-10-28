b8e397674f2165ad6073537a3296b78a6c5394f7
Joseph Delcastillo III <delcastillo3j@gmail.com>
2022-10-28 09:12:36 -0400

    Simplification for deploy testing

--- api/models/Users.js
+++ api/models/Users.js
@@ -1,20 +1,20 @@
-const db = require('../lib/DBConnection');
+// const db = require('../lib/DBConnection');
 
-const table = db.GetTable(db.TABLES.User);
+// const table = db.GetTable(db.TABLES.User);
 
-async function GetTableId() {
-    const i = table.id;
-    return i;
-}
+// async function GetTableId() {
+//     const i = table.id;
+//     return i;
+// }
 
-function Create (data) {
-    const { id } = data;
-    console.log('Create')
-    // TODO: Build Create 
-    return id; // Replace with New User ID or other reference 
-}
+// function Create (data) {
+//     const { id } = data;
+//     console.log('Create')
+//     // TODO: Build Create 
+//     return id; // Replace with New User ID or other reference 
+// }
 
-module.exports = {
-    GetTableId,
-    Create
-}
+// module.exports = {
+//     GetTableId,
+//     Create
+// }
