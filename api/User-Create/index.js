f25507fe851c463b4c8213a51d322d31e733eafc
Joseph Delcastillo III <delcastillo3j@gmail.com>
2022-10-28 15:38:23 -0400

    function testing

--- api/User-Create/index.js
+++ api/User-Create/index.js
@@ -9,36 +9,42 @@ module.exports = async function (context, req) {
                 status: 400,
                 body: "Please pass data to the request body"
             };
-        }
-
-        console.log('Start')
-        const data = req.body;
-        
-        console.log('Data Obtained')
-        console.log(data.id)
-        console.log('Running')
-        if (!data.id) { 
-            console.log('Missing Input')
-            return context.res = {
-                status: 400,
-                body: "Please pass id in the request body" + req.body
-            };
-        }
-
-        console.log('ID')
-        console.log(data.id)
-        const result = await service.Create(data);
-        console.log('result: ')
-        console.log(result)
-        if(!result) {
-            return context.res = {
-                status: 400,
-                body: "Service Failed to Execute"
-            };
         } 
-        return context.res = {
-            body: result
-        };
+        else {
+            return context.res = {
+                status: 200,
+                body: req.body
+            };
+        }
+
+        // console.log('Start')
+        // const data = req.body;
+        
+        // console.log('Data Obtained')
+        // console.log(data.id)
+        // console.log('Running')
+        // if (!data.id) { 
+        //     console.log('Missing Input')
+        //     return context.res = {
+        //         status: 400,
+        //         body: "Please pass id in the request body" + req.body
+        //     };
+        // }
+
+        // console.log('ID')
+        // console.log(data.id)
+        // const result = await service.Create(data);
+        // console.log('result: ')
+        // console.log(result)
+        // if(!result) {
+        //     return context.res = {
+        //         status: 400,
+        //         body: "Service Failed to Execute"
+        //     };
+        // } 
+        // return context.res = {
+        //     body: result
+        // };
     } catch (error) {
         return context.res = {
             status: 500,
