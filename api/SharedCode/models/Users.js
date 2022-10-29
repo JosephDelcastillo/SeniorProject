20b993aab9130b2c73f58ff3109d8640dee38d4f
Joseph Delcastillo III <delcastillo3j@gmail.com>
2022-10-28 20:06:21 -0400

    Testing Model with Dummy DB and new Trigger

--- api/SharedCode/models/Users.js
+++ api/SharedCode/models/Users.js
@@ -1,5 +1,9 @@
+// Import 
 const db = require('../lib/DBConnection');
+const db_dev = require('../lib/DBDevelopment');
 
+// Constants 
+const AUTH_ROLES = { Admin: 'Administrator', Staff: 'Staff' };
 const table = db.GetTable(db.TABLES.User);
 
 async function Create (data) {
@@ -14,6 +18,94 @@ async function Create (data) {
     });
 }
 
+async function GetStaff (search) {
+    return new Promise(resolve => {
+        console.log('Data Get Staff')
+        // Search DB For Matches  
+        const staff = (search) ? db_dev.Users.filter(({ name, email }) => strSearch(name, search) || strSearch(email, search) ) : db_dev.Users.rows;
+        console.log('Staff:')
+        console.log(staff)
+        // Format Data for Security 
+        const output = staff.map(({ id, name, email }) => { return ({ id, name, email }); }); 
+        console.log(output)
+        // Return Data 
+        resolve( output );
+    })
+}
+
+async function GetQuestion (search) {
+    return new Promise(resolve => {
+        // Search DB For Matches  
+        const question = (search) ? db_dev.Questions.filter( ({ name, is_note }) => (strSearch(name, search) && !(is_note)) ) : db_dev.Questions.filter(({is_note })=>!is_note) ;
+        // Format Data for Security 
+        const output = question.map(({ id, name }) => { return ({ id, name }); }); 
+        // Return Data 
+        resolve( output );
+    })
+}
+
+async function GetReport (people, questions, dates) {
+    return new Promise(resolve => {
+        let output = { people, questions, dates };
+        // First Get Submissions Within TimeSpan 
+        const subs = (people[0] === -202) ? db_dev.Submissions.filter(({date}) => (date >= dates.start && date <= dates.end)) : 
+            db_dev.Submissions.filter(({user, date}) => (date >= dates.start && date <= dates.end) && people.includes(user) );
+        // Next Collect Responses Based on Submissions and Questions
+        const questionsFiltered = (questions[0] === -202) ? 
+            db_dev.Questions.filter(({ is_note }) => !is_note).map(({id, name})=>{return {id, name}}) : 
+            db_dev.Questions.filter(({ id, is_note }) => !is_note && questions.includes(id)).map(({id, name})=>{return{id, name}});
+        // Next Get Responses Per the Submissions
+        const resp = db_dev.Responses.filter(({ submission, question }) => (subs.findIndex(({id}) => id === submission) >= 0) && (questionsFiltered.findIndex(q => q.id === question) >= 0));
+        // Then Get Users 
+        const pep = (people[0] === -202) ? db_dev.Users.rows.map(({ id, name }) => { return { id, name } }) : db_dev.Users.rows.filter(({id})=>people.includes(id)).map(({ id, name }) => { return { id, name } });
+        // Finally Return Information 
+        resolve({ submissions: subs, responses: resp, questions: questionsFiltered, people: pep });
+    })
+}
+
+// *** Authorization ***
+function Login ({username, password}) {
+    return Promise(resolve => {
+        let search = db.Users.rows.find(({username}) => username == username);
+        if ( typeof search === "undefined" ) {
+            resolve(false)
+        }
+
+        let user = db.Users.rows.find(({password}) => password == password);
+        if( typeof user === "undefined" ) {
+            resolve(false)
+        } 
+        resolve({ data: 'jds8a-AD78B-a79NiP-as89CNj'})
+    })
+}
+
+/**
+ * Run Authorization 
+ * @param {string} token Authorization Token
+ * @param {Function} sendFunc To Return Data
+ * @param {Function} successAction Action to do if authorized
+ */
+async function Authorize (token, requirement) {
+    return new Promise(resolve => {
+        console.log('Authorize')
+        console.log(token)
+        // TODO: Fill this in with an actual token processor
+        // Note, use the Session table to create/manage the number of users session active at one time or even limit session duration 
+        if ( token ) resolve(true);
+        
+        resolve(false)
+    })
+}
+
+// *** Helper Funcitons *** 
+const strSearch = (haystack, needle) => (haystack.toLowerCase().indexOf(needle.toLowerCase()) >= 0); 
+
 module.exports = {
+    AUTH_ROLES,
+    GetQuestion,
+    Authorize,
+    GetReport,
+    GetStaff,
+    Login,
     Create
 }
--- api/SharedCode/services/Users.js
+++ api/SharedCode/services/Users.js
@@ -20,6 +20,79 @@ async function Create (data) {
     }
 }
 
+
+// Get Questions
+async function GetQuestion(input) {
+    try {
+        const { token, data } = input;
+        const authorized = await model.Authorize(token, model.AUTH_ROLES.Staff); 
+        if(authorized) {
+            let q = await model.GetQuestion(input);
+            return q; 
+        }
+    } catch (error) {
+        context.res.status = 500;
+        context.res.body = error;
+        throw error;
+    }
+}
+
+async function GetReport(input) {
+    try {
+        const { token, data } = input;
+        const { people, questions, dates } = data;
+        if (Array.isArray(people)) {
+            const authorized = await model.Authorize( token, model.AUTH_ROLES.Admin); 
+            if(!authorized) return false;
+            
+            const report = await model.GetReport( people, questions, dates );
+            if(report) return false;
+            return report;
+        } else {
+            const authorized = await model.Authorize( token, model.AUTH_ROLES.Staff); 
+            if(!authorized) return false;
+            
+            const report = await model.GetReport([ people ], questions, dates );
+            if(report) return false;
+            return report;
+        }
+    } catch (error) {
+        return false;
+    }
+}
+
+async function GetStaff(input) {
+        const { token, data } = input;
+        console.log(token); 
+    try {
+        console.log('Authorizing'); 
+        const authorized = await model.Authorize( token, model.AUTH_ROLES.Admin); 
+        console.log('Authorization'); 
+        console.log(authorized); 
+        if(!authorized) return false;
+        
+        console.log('Authorized')
+        const staff = await model.GetStaff(data.search);
+        console.log('Staff Collected')
+        if(staff) return staff;
+        return false;
+    } catch(error) {
+        return false;
+    }
+}
+
+async function Login (username, password) {
+    try {
+        return model.Login({ username, password });
+    } catch (error) {
+        return false; 
+    }
+}
+
 module.exports = {
+    GetQuestion,
+    GetReport,
+    GetStaff,
+    Login,
     Create
 }
