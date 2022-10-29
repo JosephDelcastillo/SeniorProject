83ab6b14026edd0edbd55c595acfdaddd8b6c094
Joseph Delcastillo III <delcastillo3j@gmail.com>
2022-10-28 20:06:21 -0400

    Testing Model with Dummy DB and new Trigger

---
+++ api/GetStaff/index.js
@@ -0,0 +1,45 @@
+// Imports 
+const service = require('../SharedCode/services/Users');
+
+module.exports = async function (context, req) {
+    context.log('JavaScript HTTP trigger function processed a request.');
+    if (!req.body) {
+        return context.res = {
+            status: 400,
+            body: "Please pass data to the request body"
+        };
+    } 
+
+    try {
+        console.log('Start')
+        const data = req.body;
+        
+        console.log('Data Obtained')
+        if (!data.data) { 
+            console.log('Missing Input')
+            return context.res = {
+                status: 400,
+                body: "Please pass id in the request body" + req.body
+            };
+        }
+
+        console.log('Start Get Staff')
+        const result = await service.GetStaff(data);
+        console.log('result: ')
+        console.log(result)
+        if(!result) {
+            return context.res = {
+                status: 400,
+                body: "Service Failed to Execute"
+            };
+        } 
+        return context.res = {
+            body: result
+        };
+    } catch (error) {
+        return context.res = {
+            status: 500,
+            body: error 
+        };
+    }
+}
