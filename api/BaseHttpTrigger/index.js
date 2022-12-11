6392275af51f210644f563f39c1ee040e570216e
Joseph Delcastillo III <delcastillo3j@gmail.com>
2022-12-11 14:05:34 -0500

    Further Key Probing

--- api/BaseHttpTrigger/index.js
+++ api/BaseHttpTrigger/index.js
@@ -1,7 +1,9 @@
 module.exports = async function (context, req) {
     context.log.verbose('Basic Function Triggered ');
-    context.log.verbose(process.env["URI"]);
-    context.log.verbose(process.env["COSMOS_ENDPOINT"]);
+    context.log.verbose('U' + process.env["URI"]);
+    context.log.verbose('K' + process.env["PRIMARY KEY"]);
+    context.log.verbose('E' + process.env["COSMOS_ENDPOINT"]);
+    context.log.verbose('C' + process.env["COSMOS_KEY"]);
     context.log('JavaScript HTTP trigger function processed a request.');
 
     const name = (req.query.name || (req.body && req.body.name));
