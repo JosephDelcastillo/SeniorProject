93d2d6e12d59cfc5d1d59aed7eb38b5e46492108
Joseph Delcastillo III <delcastillo3j@gmail.com>
2022-12-11 17:32:55 -0500

    Fix Previous Bug

--- api/SharedCode/models/DB.js
+++ api/SharedCode/models/DB.js
@@ -3,8 +3,8 @@ const { Users } = require('../lib/DBConnection');
 // Test
 async function Test (data) {
     return new Promise(async resolve => {
-        resolve ({ Users });
-        return { Users };
+        resolve ({ Users: Users.id });
+        return { Users: Users.id };
     });
 }
 
