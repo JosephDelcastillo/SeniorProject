5ae11876fdf709e6227b07314db35d83588fda39
Joseph Delcastillo III <delcastillo3j@gmail.com>
2023-02-03 16:18:38 -0500

    Simplify api helper

--- client/src/App.js
+++ client/src/App.js
@@ -2,8 +2,6 @@ import React from 'react';
 import NavBar from './Components/NavBar';
 import RouteController from "./Controller/RouteController";
 
-import { call } from './Controller/API';
-
 const LOCAL_STORAGE_KEY = 'janfl-epots.user';
 
 function setToken(userToken) { sessionStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(userToken)); }
@@ -26,7 +24,31 @@ function isAdmin () {
   * @param {string} action  POST or GET | Defaults to POST
  */
 async function api({ func, data, action }) {
-  return await call(getToken, { func, data, action });
+  //********  Step 1: Initialize Data  ********
+  // Prepare URL
+  const API_URL = `/api`
+  // Get Post vs Get
+  const IS_POST = action ? (action.toLowerCase() !== 'get') : true ; // Default to POST 
+
+  // Setup Url
+  let url = `/${func}`;
+  if(!IS_POST){
+      url += "?";
+      // Cycle through all key value pairs of data and converting them into a GET query
+      for(const [key, value] of Object.entries(data)){
+          url += `${key}=${value}&`;
+      }
+      // Remove Last '&'
+      url = url.slice(0, -1);
+  }
+  // Setup Data (With Token Possible)
+  const DATA = getToken() ? { token: JSON.parse(getToken()), data } : data;
+
+  if(IS_POST){
+      return await axios.post(API_URL + url, DATA).then(response => response.data)
+  } else {
+      return await axios.get(API_URL + url).then(response => response.data)
+  }
 }
 
 function App() { 
